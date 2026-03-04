360d28275798627b5e5c5a487bf1793b90a71431
mindmeister10 <aaarama21@gmail.com>
2026-03-04 21:54:49 +0700

    Update latihan4.cpp

--- praktikum_4/latihan4.cpp
+++ praktikum_4/latihan4.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
 
+int main() {
+    std::vector<double>sensor;
+    std::ofstream file("hasil_kalibrasi.txt");
+    sensor.push_back(100.5);
+    sensor.push_back(200.0);
+    sensor.push_back(150.5);
+    for(auto &i : sensor) {
+        i *= 1.1;
+    }
+
+    if(file.is_open()) {
+        for(int i = 0; i < 3; i++) {
+            file << "Hasil kalibrasi data ke-" << i + 1 << " : " << sensor[i] << '\n';
+        }
+        file.close();
+    } else {
+        std::cout << "Gagal membuka file" << '\n';
+    }
+}
